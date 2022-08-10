203c60de92b21ce7ce38c89ee445ae7c27941ac3
Pratik <pratikmmaniya244@gmail.com>
2022-08-10 12:16:24 -0400

    changed UI

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -16,7 +16,7 @@ function ControlledCarousel(props) {
                 props.movies && props.movies.length > 0
                     ?
                     props.movies.map(movie => (
-                        <Carousel.Item style={{ height: '700px', cursor: 'pointer' }} interval={4000} key={movie.id} onClick={() => { navigate(process.env.PUBLIC_URL + '/movie-details/' + movie.id) }}>
+                        <Carousel.Item style={{ height: '650px', cursor: 'pointer' }} interval={4000} key={movie.id} onClick={() => { navigate(process.env.PUBLIC_URL + '/movie-details/' + movie.id) }}>
                             <img
                                 className="d-block w-100"
                                 src={movie.largePoster}
